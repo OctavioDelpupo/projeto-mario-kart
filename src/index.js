6d10a256b2ccfd9a08b3a424f397c78967c1be2d
Octavio Delpupo <octavio.delpupo@gmail.com>
2025-06-26 21:21:15 -0300

    feat: function of rolling a die.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ const player1 = {
   VELOCIDADE: 4,
   MANOBRABILIDADE: 3,
   PODER: 3,
-  PONTOS: 0
-}
+  PONTOS: 0,
+};
 
 const player2 = {
   NOME: "LUIGI",
   VELOCIDADE: 3,
   MANOBRABILIDADE: 4,
   PODER: 4,
-  PONTOS: 0
-}
+  PONTOS: 0,
+};
 
+async function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
