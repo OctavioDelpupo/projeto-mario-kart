fb24d5473dc1d8bfe765c9dae4cea31d829ee60c
Octavio Delpupo <octavio.delpupo@gmail.com>
2025-06-26 21:07:20 -0300

    feat:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Olá mundo");
+const player1 = {
+  NOME: "MARIO",
+  VELOCIDADE: 4,
+  MANOBRABILIDADE: 3,
+  PODER: 3,
+  PONTOS: 0
+}
+
+const player2 = {
+  NOME: "LUIGI",
+  VELOCIDADE: 3,
+  MANOBRABILIDADE: 4,
+  PODER: 4,
+  PONTOS: 0
+}
+
